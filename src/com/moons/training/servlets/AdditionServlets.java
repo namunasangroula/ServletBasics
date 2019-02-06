baeab76232b9631ff440152a5f9d7f8da986e8a7
Namuna Sangroula <namunasangroula@gmail.com>
2019-02-06 17:13:39 -0600

    My first server

---
+++ src/com/moons/training/servlets/AdditionServlets.java
@@ -0,0 +1,28 @@
+package com.moons.training.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class AdditionServlets extends GenericServlet {
+
+	@Override
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		if(request.getParameter("number1")!=null && request.getParameter("number2")!=null) {
+		
+		Long num1 = Long.parseLong(request.getParameter("number1"));
+		Long num2 = Long.parseLong(request.getParameter("number2"));
+
+			PrintWriter out = response.getWriter();
+			out.println("The result is: " + (num1 + num2));
+		
+		}
+		
+		
+	}
+
+}
